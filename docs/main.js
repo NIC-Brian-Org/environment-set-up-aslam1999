c7633ff05f801a74486850c993eb5cf1b45c6ced
aslam1999 <aslamhotmail@gmail.com>
2025-09-06 17:01:38 -0700

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -1,16 +1,17 @@
-console.log('main.js');
+console.log("main.js");
 (() => {
-    console.log('Here we are!')
-    window.addEventListener("load", (event) => {
-        const multiplier = document.getElementById( 'multiplier' );
-        const multiplicand = document.getElementById( 'multiplicand' );
-        const product = document.getElementById( 'product' );
+  console.log("Here we are!");
+  window.addEventListener("load", (event) => {
+    const number1 = document.getElementById("number1");
+    const number2 = document.getElementById("number2");
+    const product = document.getElementById("product");
 
-        const update = () => {
-            product.innerHTML = parseFloat( multiplier.value ) * parseFloat( multiplicand.value );
-        };
+    const update = () => {
+      product.innerHTML =
+        parseFloat(multiplier.value) * parseFloat(multiplicand.value);
+    };
 
-        multiplier.addEventListener("input", update);
-        multiplicand.addEventListener("input", update);
-    });      
+    multiplier.addEventListener("input", update);
+    multiplicand.addEventListener("input", update);
+  });
 })();
